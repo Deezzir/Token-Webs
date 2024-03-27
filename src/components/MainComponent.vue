7e0037b5c14e23118717035b941a942837492a0f
Iurii Kondrakov <deezzir@gmail.com>
2024-03-27 17:49:51 -0400

    Add club token

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,40 +1,46 @@
 <script setup lang="ts">
 import { TwitterIcon, TelegramIcon } from '@/components/icons'
+import DocsIcon from './icons/DocsIcon.vue'
 </script>
 
 <template>
-  <div class="flex flex-col relative place-items-center">
+  <div class="flex flex-col relative place-items-center px-20">
     <img
       alt="jewno"
       fetchpriority="high"
-      width="300"
-      height="300"
+      width="600"
+      height="600"
       decoding="async"
       data-nimg="1"
       class="relative rounded-lg"
       style="color: transparent"
-      src="../assets/jewno.png"
+      src="../assets/pump2.jpg"
     />
     <p
       class="[word-spacing:-1px] sm:[word-spacing:0px] relative text-2xl sm:text-4xl text-center font-bold pt-10 pb-1 text-[#14F195] drop-shadow-[0_3px_3px_rgba(153,69,255,0.7)]"
     >
-      $JEWNOS - Jewnosaurs Coin
+      $PUMPCLUB
     </p>
     <p class="relative text-xl sm:text-2xl text-center pb-5">
-      Can't wait to buy $JEWNOS on #Solana and for it to go to the mooon
+      $PUMPCLUB is a cryptocurrency launched on the Solana blockchain through the website pump.fun.
+      The project aims to create a long-term hold investment opportunity for its community members.
+      The team behind PumpClub plans to launch a new coin every 3-4 days, with profits flowing back
+      into the native $PUMPCLUB token. This strategy has the potential to drive continuous community
+      growth and engagement.
     </p>
     <p class="relative text-xl sm:text-2xl text-center pb-5">
-      Lets gooo! Live on Raydium 🚀🚀🚀
+      Support on
       <a
         href="https://dexscreener.com/solana/E8YNxNTq4s21rU2Lhxt9VzkMzXxd8C4i81VUk6qd9Sqs"
         class="text-blue-500 hover:text-blue-700 transition duration-150 ease-in-out"
       >
-        DEX SCREENER
+        Pump.fun
       </a>
+      🚀🚀🚀
     </p>
     <div class="flex flex-row justify-center items-center pt-10">
       <a
-        href="https://x.com/JEWNOSAURS"
+        href="https://x.com/pumpfunclub"
         target="_blank"
         rel="noopener noreferrer"
         class="bg-[#14F195] hover:bg-[#f7be47] text-[#181818] font-bold text-4xl p-2 rounded mx-4"
@@ -42,12 +48,20 @@ import { TwitterIcon, TelegramIcon } from '@/components/icons'
         <TwitterIcon />
       </a>
       <a
-        href="https://t.me/jewnosaurs"
+        href="https://t.me/PumpFunClub"
         target="_blank"
         rel="noopener noreferrer"
         class="bg-[#14F195] hover:bg-[#f7be47] text-[#181818] font-bold text-4xl p-2 rounded mx-4"
       >
         <TelegramIcon />
+      </a>
+      <a
+        href="https://cdn.discordapp.com/attachments/1219010663117226151/1222663102085202013/PumpClubWhitePaper_.pdf?ex=661708bf&is=660493bf&hm=16d544e5229fdb29ea6909a064ed94cec7be61988d539acf3975669a019aa6e0&"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="bg-[#14F195] hover:bg-[#f7be47] text-[#181818] font-bold text-4xl p-2 rounded mx-4"
+      >
+        <DocsIcon />
       </a>
     </div>
   </div>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as TelegramIcon } from './TelegramIcon.vue';
 export { default as TwitterIcon } from './TwitterIcon.vue';
 export { default as CopyIcon } from './CopyIcon.vue';
+export { default as DocsIcon } from './DocsIcon.vue';
