b57d26815ef11b53669edd643e2a47c248cba4fb
Iurii Kondrakov <deezzir@gmail.com>
2024-03-29 23:11:55 -0400

    Update globals

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import router from './router'
 
 const app = createApp(App)
 
-app.config.globalProperties.$CA = 'Coming'
+app.config.globalProperties.$CA = 'CVkbsGP3d5HSHYSrR6mmPMh4BsT5qK4rQd7h51TUL8mq'
 app.config.globalProperties.$telegram = 'https://t.me/catwafhat'
 app.config.globalProperties.$twitter = 'https://twitter.com/catwafhat'
-app.config.globalProperties.$buyLink = 'https://www.pump.fun/board'
+app.config.globalProperties.$buyLink = 'https://www.pump.fun/CVkbsGP3d5HSHYSrR6mmPMh4BsT5qK4rQd7h51TUL8mq'
 
 app.use(router)
 
