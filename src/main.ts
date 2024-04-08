744bbd74cae79df0db1052b186a2bc409120742d
Iurii Kondrakov <deezzir@gmail.com>
2024-04-08 01:58:46 -0400

    Update CA

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import router from './router'
 
 const app = createApp(App)
 
-app.config.globalProperties.$CA = 'Coming'
+app.config.globalProperties.$CA = 'Ezz6s3KGxbyxDuWxU8Tbbx8EfrNXuUnoYySx5YFfpDRC'
 app.config.globalProperties.$telegram = 'https://t.me/gandalfparty'
 app.config.globalProperties.$twitter = 'https://twitter.com/GANDALFPARTY'
-app.config.globalProperties.$buyLink = ''
+app.config.globalProperties.$buyLink = 'https://www.pump.fun/Ezz6s3KGxbyxDuWxU8Tbbx8EfrNXuUnoYySx5YFfpDRC'
 
 app.use(router)
 
