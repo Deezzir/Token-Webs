07c1f3f1f0b85ecea0ff7c9efc9bf4c4427aca38
Iurii Kondrakov <deezzir@gmail.com>
2024-04-10 00:11:18 -0400

    Update links

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.config.globalProperties.$CA = 'Coming'
+app.config.globalProperties.$CA = 'DA4EQzj6GHgAS7i9ffia1c35NyHqqrnHdfp9A55HZuVq'
 app.config.globalProperties.$telegram = 'https://t.me/frodo_frogdog'
 app.config.globalProperties.$twitter = 'https://twitter.com/FRODOTHEFROGDOG'
-app.config.globalProperties.$buyLink = 'https://www.pump.fun/'
+app.config.globalProperties.$buyLink = 'https://pump.fun/DA4EQzj6GHgAS7i9ffia1c35NyHqqrnHdfp9A55HZuVq'
 app.config.globalProperties.$telegramDev = 'https://t.me/lettodev'
-app.config.globalProperties.$dex = 'https://dexscreener.com/solana/'
+app.config.globalProperties.$dex = 'https://dexscreener.com/solana/DA4EQzj6GHgAS7i9ffia1c35NyHqqrnHdfp9A55HZuVq'
 
 app.mount('#app')
