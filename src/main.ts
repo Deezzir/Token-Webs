8c96060ef8f5fb6f32b7a826f513ea9412c4272f
Iurii Kondrakov <deezzir@gmail.com>
2024-04-10 03:37:40 -0400

    Update buy link

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const app = createApp(App)
 app.config.globalProperties.$CA = 'DA4EQzj6GHgAS7i9ffia1c35NyHqqrnHdfp9A55HZuVq'
 app.config.globalProperties.$telegram = 'https://t.me/frodo_frogdog'
 app.config.globalProperties.$twitter = 'https://twitter.com/FRODOTHEFROGDOG'
-app.config.globalProperties.$buyLink = 'https://pump.fun/DA4EQzj6GHgAS7i9ffia1c35NyHqqrnHdfp9A55HZuVq'
+app.config.globalProperties.$buyLink = 'https://raydium.io/swap/?inputCurrency=DA4EQzj6GHgAS7i9ffia1c35NyHqqrnHdfp9A55HZuVq&outputCurrency=sol&fixed=in'
 app.config.globalProperties.$telegramDev = 'https://t.me/lettodev'
 app.config.globalProperties.$dex = 'https://dexscreener.com/solana/2xCGqSn86Y2Ziba7sR6Nd2G5HqaYtFfL5NhpvwsyKc9R'
 
