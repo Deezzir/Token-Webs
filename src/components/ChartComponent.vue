65b99e469beffb1eb66c1d3828cd231348db80f1
Iurii Kondrakov <deezzir@gmail.com>
2024-04-10 00:45:18 -0400

    Update chart

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -2,8 +2,8 @@
   <div
     class="relative flex min-h-screen items-center justify-center overflow-hidden bg-[#384a28] px-12 py-16 text-4xl text-white sm:text-5xl xl:px-[15rem]"
   >
-    <div class="flex flex-col items-center justify-center gap-16">
-      <img src="@/assets/bag.png" alt="Bag" class="w-[80%] sm:w-[60%]" />
+    <div class="flex w-full flex-col items-center justify-center gap-16">
+      <img src="@/assets/bag.png" alt="Bag" class="w-[80%] sm:w-[30%]" />
       <div id="dexscreener-embed">
         <iframe
           src="https://dexscreener.com/solana/2xCGqSn86Y2Ziba7sR6Nd2G5HqaYtFfL5NhpvwsyKc9R?embed=1&theme=dark"
