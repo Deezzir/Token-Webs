4feeda81fa78c045bcbfefee35bd04f96c56f1f2
Iurii Kondrakov <deezzir@gmail.com>
2024-04-13 01:40:31 -0400

    Update buys

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.config.globalProperties.$CA = 'Coming'
+app.config.globalProperties.$CA = '7rwHQ8qykg4AjP6Tv5Xo6HPUVdug2gA2LcK9y8wWHU22'
 app.config.globalProperties.$telegram = 'https://t.me/thevault_boy'
 app.config.globalProperties.$twitter = 'https://twitter.com/VAULTBOYI'
-app.config.globalProperties.$buyLink = 'Coming'
+app.config.globalProperties.$buyLink = 'https://www.pump.fun/7rwHQ8qykg4AjP6Tv5Xo6HPUVdug2gA2LcK9y8wWHU22'
 app.config.globalProperties.$telegramDev = 'https://t.me/lettodev'
 app.config.globalProperties.$dex = 'Coming'
 app.config.globalProperties.$raydium = 'Coming'
