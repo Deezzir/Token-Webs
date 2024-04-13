5f42f51fd64a5acd3a88378b02408a62b27a98d2
Iurii Kondrakov <deezzir@gmail.com>
2024-04-13 01:10:18 -0400

    Update links and meta

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import App from './App.vue'
 const app = createApp(App)
 
 app.config.globalProperties.$CA = 'Coming'
-app.config.globalProperties.$telegram = 'Coming'
-app.config.globalProperties.$twitter = 'Coming'
+app.config.globalProperties.$telegram = 'https://t.me/thevault_boy'
+app.config.globalProperties.$twitter = 'https://twitter.com/VAULTBOYI'
 app.config.globalProperties.$buyLink = 'Coming'
 app.config.globalProperties.$telegramDev = 'https://t.me/lettodev'
 app.config.globalProperties.$dex = 'Coming'
