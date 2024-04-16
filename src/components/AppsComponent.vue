cdc37b2e2c8ff17d868c9e1ba9ef7e16a97fbb57
Iurii Kondrakov <deezzir@gmail.com>
2024-04-16 18:54:12 -0400

    Fix a tags

--- src/components/AppsComponent.vue
+++ src/components/AppsComponent.vue
@@ -28,49 +28,75 @@ export default {
   <div
     class="grid h-[70%] grid-flow-row items-start justify-center gap-x-1 gap-y-3 text-sm sm:grid-cols-2 sm:grid-rows-4"
   >
-    <a class="flex flex-col items-center gap-1" href="https://pump.fun/board"
+    <a
+      class="flex flex-col items-center gap-1"
+      href="https://pump.fun/board"
+      target="_blank"
+      rel="noopener noreferrer"
       ><img src="@/assets/bin.png" alt="recycle bin" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Recycle Bin</span
       ></a
     >
-    <a class="flex flex-col items-center gap-1" :href="telegramDev"
+    <a
+      class="flex flex-col items-center gap-1"
+      :href="telegramDev"
+      target="_blank"
+      rel="noopener noreferrer"
       ><img src="@/assets/my-computer.png" alt="dev" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Dev</span
       ></a
     >
-    <a class="flex flex-col items-center gap-1" :href="buyLink"
+    <a
+      class="flex flex-col items-center gap-1"
+      :href="buyLink"
+      target="_blank"
+      rel="noopener noreferrer"
       ><img src="@/assets/coin.png" alt="dev" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Buy</span
       ></a
     >
-    <a class="flex flex-col items-center gap-1" :href="telegram"
+    <a
+      class="flex flex-col items-center gap-1"
+      :href="telegram"
+      target="_blank"
+      rel="noopener noreferrer"
       ><img src="@/assets/tg.png" alt="telegram" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Telegram</span
       ></a
     >
-    <a class="flex flex-col items-center gap-1" :href="twitter"
+    <a
+      class="flex flex-col items-center gap-1"
+      :href="twitter"
+      target="_blank"
+      rel="noopener noreferrer"
       ><img src="@/assets/x.png" alt="twitter" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >X</span
       ></a
     >
     <!-- <a class="flex flex-col items-center gap-1" :href="dex"
+        target="_blank"
+      rel="noopener noreferrer"
       ><img src="/icons/dex.png" alt="logo" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Dex</span
       ></a
     >
     <a class="flex flex-col items-center gap-1" :href="dex"
+    target="_blank"
+      rel="noopener noreferrer"
       ><img src="/icons/raydium.png" alt="logo" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Raydium</span
       ></a
     >
     <a class="flex flex-col items-center gap-1" :href="dex"
+    target="_blank"
+      rel="noopener noreferrer"
       ><img src="/icons/jupiter.png" alt="logo" class="h-10 w-10" /><span
         class="min-w-24 bg-[#008080] text-center"
         >Jupiter</span
