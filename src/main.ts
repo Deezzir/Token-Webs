960d285074dce3e576fb2a8269d90548a69115b0
Iurii Kondrakov <deezzir@gmail.com>
2024-04-16 19:53:46 -0400

    Update buy links

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.config.globalProperties.$CA = 'Coming'
+app.config.globalProperties.$CA = '4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL'
 app.config.globalProperties.$telegram = 'https://t.me/mihabibi11'
 app.config.globalProperties.$twitter = 'https://twitter.com/mihabibi_'
-app.config.globalProperties.$buyLink = 'https://pump.fun/'
+app.config.globalProperties.$buyLink = 'https://pump.fun/4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL'
 app.config.globalProperties.$telegramDev = 'https://t.me/lettodev'
 app.config.globalProperties.$dex = 'Coming'
 app.config.globalProperties.$raydium = 'Coming'
